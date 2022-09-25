f9a8ddc8624231694f4fa9d57e36e47b4b3fe94d
Ale Mendoza <aletinmendoza@hotmail.com>
2022-09-25 15:59:59 -0600

    QR connected

--- Semillita/BuscarQRViewController.swift
+++ Semillita/BuscarQRViewController.swift
@@ -13,20 +13,36 @@ class BuscarQRViewController: UIViewController {
     
     @IBOutlet weak var buscarPlanta: UITextField!
     let detallesPlantaServicio = PlantaService()
+    var plant: Planta?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         }
     
+    init() {
+        plant = nil
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+       super.init(coder: aDecoder)
+    }
     
     @IBAction func buscarQR(_ sender: UIButton) {
         if buscarPlanta != nil {
-        detallesPlantaServicio.leerPlanta(plantaId: Int(buscarPlanta.text!)!){
+            detallesPlantaServicio.readPlant(nombre: buscarPlanta.text!) {
             (plantaRecibida) in
             guard plantaRecibida != nil else { print("No se armo"); return }
+                self.plant = plantaRecibida
                 self.performSegue(withIdentifier: "BuscarQR_To_ImprimirQR", sender: self)
             }
         }
     }
-}
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "BuscarQR_To_ImprimirQR",
+           let secondViewController = segue.destination as? ImprimirQRViewController {
+            secondViewController.plant = plant
+            }
+        }
+    }
--- Semillita/ImprimirQRViewController.swift
+++ Semillita/ImprimirQRViewController.swift
@@ -11,14 +11,33 @@ import Alamofire
 
 class ImprimirQRViewController: UIViewController {
 
+    @IBOutlet weak var nombre_tradicional: UILabel!
+    @IBOutlet weak var nombre_cientifico: UILabel!
+    @IBOutlet weak var imagen: UIImageView!
+    
+    var plant: Planta?
     let imprimirQRService = ImprimirQRService()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        imprimirQRService.imprimirQR(planta_id: 75) {
+        nombre_tradicional.text = plant?.nombre_tradicional
+        nombre_cientifico.text = plant?.nombre_cientifico
+        
+        let imagefunctions = ImageFunctions()
+        let new_image:UIImage = imagefunctions.convert(base64: (plant?.Pimagenes![0].dato)!)
+        imagen.image = new_image
+        imprimirQRService.imprimirQR(planta_id: (plant?.id!)!) {
             (plantaRecibida) in
             print(plantaRecibida)
         }
+    }
+    init() {
+        plant = nil
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+       super.init(coder: aDecoder)
     }
 }
